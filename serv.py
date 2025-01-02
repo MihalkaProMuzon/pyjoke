f957be365f164961343d0665eb12214cfeb28bbe
MihalkaProMuzon <NeOchHoroho2001@mail.ru>
2025-01-02 16:46:27 +0400

    teseter

--- serv.py
+++ serv.py
@@ -13,7 +13,7 @@ CODER = "utf-8"
 LOCAL_ADDR = ('85.192.26.114', 52600)
 #LOCAL_ADDR = ('127.0.0.1', 52600)
 
-PROJ_VERSION = "v.main.5"
+PROJ_VERSION = "v.main.5 test brach"
 
 PYTHON_PATH = '../../python'
 
