21b5f5d2c9cd86253d7022d4120fe68f7d3966aa
MihalkaProMuzon <NeOchHoroho2001@mail.ru>
2025-01-02 16:21:13 +0400

    branch switch check

--- serv.py
+++ serv.py
@@ -13,7 +13,7 @@ CODER = "utf-8"
 LOCAL_ADDR = ('85.192.26.114', 52600)
 #LOCAL_ADDR = ('127.0.0.1', 52600)
 
-PROJ_VERSION = "v.updater-ficha.4 priiivvved"
+PROJ_VERSION = "v.updater-ficha.4 branch switch check"
 
 PYTHON_PATH = '../../python'
 
