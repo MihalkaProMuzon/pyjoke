7ed74fceb61744050d2ddc90b9d46dfd15767ecd
MihalkaProMuzon <NeOchHoroho2001@mail.ru>
2025-01-03 15:16:05 +0400

    cod test

--- serv.py
+++ serv.py
@@ -13,7 +13,9 @@ CODER = "utf-8"
 LOCAL_ADDR = ('85.192.26.114', 52600)
 #LOCAL_ADDR = ('127.0.0.1', 52600)
 
-PROJ_VERSION = "v.testered.1 dla testov"
+PROJ_VERSION = "v.testered.2 dla testov"
+print("КОд подменялся Ызы")
+
 
 PYTHON_PATH = '../../python'
 
